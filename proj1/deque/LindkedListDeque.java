fedb617e449a0084063e94d3886c395dfac5b8ab
khaled <khaledlejri23@gmail.com>
2024-09-14 13:31:45 +0200

    Implemented addFirst method to handle circular sentinel structure in a deque

---
+++ proj1/deque/LindkedListDeque.java
@@ -0,0 +1,57 @@
+package deque;
+
+public class LindkedListDeque<ItemType> {
+    private int size;
+    private IntNode sentinel;
+
+    public LindkedListDeque(){
+        size = 0;
+        sentinel = new IntNode(null, null, null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+    }
+
+    // inner class to represent nodes in the deque
+    private class IntNode{
+        public ItemType item;
+        public IntNode prev;
+        public IntNode next;
+
+        public IntNode(ItemType i, IntNode p, IntNode n){
+            item = i;
+            prev = p;
+            next = n;
+        }
+    }
+
+    //public ItemType get(int index){}
+
+    /** Adds a first item to the deque,
+     *  Differentiates between having empty deque and not so we can keep the circular sentinel.  */
+    public void addFirst(ItemType x){
+        IntNode newNode = new IntNode(x, sentinel, sentinel.next);
+        if(size == 0){
+            sentinel.prev = sentinel.next = newNode;
+        } else {
+            sentinel.next = newNode;
+            sentinel.next.next.prev = newNode;
+        }
+        size += 1;
+    }
+
+    public void addLast(ItemType x){
+
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+//
+//    public itemType remove(ItemType x){}
+//
+//    public itemType getRecursive(int index)
+//    public int size(){}
+
+
+}
---
+++ proj1/deque/LinkedListDequeLauncher.java
@@ -0,0 +1,9 @@
+package deque;
+
+public class LinkedListDequeLauncher {
+    public static void main(String[] args){
+        LindkedListDeque<String> s1 = new LindkedListDeque<>();
+        s1.addFirst("khaled");
+        s1.addFirst("teresa");
+    }
+}
